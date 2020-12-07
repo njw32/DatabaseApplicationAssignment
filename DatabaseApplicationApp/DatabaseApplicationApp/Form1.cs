95eb230d2620300e4bd9da589c499370d41e6eb1
Nathan Walter <njw32@calvin.edu>
2020-12-07 15:00:39 -0500

    pulls from db and displays author table

---
+++ DatabaseApplicationApp/DatabaseApplicationApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseApplicationApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void authorsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.authorsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.pubsDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'pubsDataSet.authors' table. You can move, or remove it, as needed.
+            this.authorsTableAdapter.Fill(this.pubsDataSet.authors);
+
+        }
+    }
+}
