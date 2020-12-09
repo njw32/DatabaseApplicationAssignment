d334f502c2aa0b998db2ceb5da6dc0ec2e51ed8d
Nathan Walter <njw32@calvin.edu>
2020-12-08 21:50:53 -0500

    read from db and write to textBox

---
+++ AuthorsApplication/AuthorsApplication/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace AuthorsApplication
+{
+    partial class AuthorsApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listAuthors = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listAuthors
+            // 
+            this.listAuthors.FormattingEnabled = true;
+            this.listAuthors.Location = new System.Drawing.Point(27, 12);
+            this.listAuthors.Name = "listAuthors";
+            this.listAuthors.Size = new System.Drawing.Size(292, 355);
+            this.listAuthors.TabIndex = 0;
+            this.listAuthors.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // AuthorsApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 394);
+            this.Controls.Add(this.listAuthors);
+            this.Name = "AuthorsApp";
+            this.Text = "Authors App";
+            this.Load += new System.EventHandler(this.AuthorsApp_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listAuthors;
+    }
+}
+
---
+++ AuthorsApplication/AuthorsApplication/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AuthorsApplication
+{
+    public partial class AuthorsApp : Form
+    {
+        public AuthorsApp()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void AuthorsApp_Load(object sender, EventArgs e)
+        {
+            loadData();
+
+        }
+
+        private void loadData()
+        {
+            string cn_string = Properties.Settings.Default.pubsConnectionString;
+
+            SqlConnection cn_connection = new SqlConnection(cn_string);
+
+            if (cn_connection.State != ConnectionState.Open) cn_connection.Open();
+
+            string sql_Text = "SELECT au_lname, au_fname, address FROM authors";
+
+            DataTable tbl = new DataTable();
+
+            SqlDataAdapter adapter = new SqlDataAdapter(sql_Text, cn_connection);
+            adapter.Fill(tbl);
+
+          
+            //show elements
+            listAuthors.DisplayMember = "au_lname";
+            listAuthors.ValueMember = "au_id";
+
+            listAuthors.DataSource = tbl;
+        }
+    }
+}
